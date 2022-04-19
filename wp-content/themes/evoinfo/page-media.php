c6d3c18320e078adbf8709c513b7b66804b34794
=Kaung Htet Kyaw <khtetkyaw.cr@gmail.com>
2022-04-19 22:03:17 +0630

    added functions

---
+++ wp-content/themes/evoinfo/page-media.php
@@ -0,0 +1,295 @@
+<?php get_header(); ?>
+  <section class="bg_green_round">
+      <div class="site_wrapper indent_content">
+       <!--//globalNav-->
+       <div class="fix col_2 mb40">
+
+           <div class="left_area sp_mb15">
+               <div class="line_dot">
+                   <p class="f_green">OurService 01</p>
+                   <p class="f_32 f_bold ls_2 f_green">MiRAKUU</p>
+               </div>
+               <p class="f_20 f_bold mb15">保育園・幼稚園の魅力が満載の専門誌＆情報サイト</p>
+               <p class="lh_2 mb15">1都3県にある保育園・幼稚園約12,000施設に配布している業界誌です。<br>
+                   保育科のある専門学校にも配布しています。<br>
+                   イケメン俳優の1日保育士体験・著名人との対談・保育園・幼稚園取材・先生に役立つ記事・保育を支えている企業の紹介など様々な情報を園向けに発信しています。<br>
+                   また、保育園・幼稚園の園長先生へ貴社の商品やサービスを広告や記事を用いてPRすることが出来ます。
+               </p>
+               <p class="seminar_btn t_right"><a href="/media/mirakuu.php" class="f_green">MORE
+                       →</a></p>
+           </div>
+           <div class="right_area">
+               <p><img src="wp-content/themes/template03/img/top/img_03.jpg" alt="MiRAKUU"></p>
+           </div>
+       </div>
+       <div class="mb80">
+           <ul class="col_4 fix mb10" id="backnumber">
+
+
+               <li class="matchHeight"> <a href="https://www.mirakuupremium.com/mirakuu/mirakuu38/" target="_blank">
+                 <?php
+                     $the_query = new WP_Query( array(
+                       'post_type' => 'mirakuu',
+                        'posts_per_page' => 4,
+                     ));
+                  ?>
+
+                       <div class="fix col_1_1 mb20">
+                           <div class="left_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/MIRAIKU_vol38.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                           </div>
+                           <div class="right_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/MIRAIKU_vol38-guest.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                           </div>
+                       </div>
+
+
+                       <p class="f_20 f_bold f_green mb10">
+                           MiRAKUU Vol.38 </p>
+                       <p class="mb10">2022年04月15日 発行</p>
+                       <p class="f_bold">【主なトピック】</p>
+                       <div class="f_12">
+       ★イケメン俳優の1日保育士体験★久保田秀敏
+       ★SDGsに取り組む保育園特集
+       ★編集長対談シリーズ★タレント・体操インストラクター　小林よしひささん
+       ★【新連載】＼ノガミキ流／保育士働き方改革委員会
+       ★【新連載】警告！　今のままでは保育園・幼稚園の「音」環境に問題あり！？　「保育環境と音」の専門家が語る子どもの「耳」の発達に悪い影響を与えている要因とは！？                                    </div>
+                   </a>
+               </li>
+
+               <li class="matchHeight"> <a href="https://www.mirakuupremium.com/mirakuu/mirakuu37/" target="_blank">
+
+                       <div class="fix col_1_1 mb20">
+                           <div class="left_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/MIRAIKU_vol37.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                           </div>
+                           <div class="right_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/MIRAIKU_vol37-guest.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                           </div>
+                       </div>
+
+
+                       <p class="f_20 f_bold f_green mb10">
+                           MiRAKUU Vol.37 </p>
+                       <p class="mb10">2022年01月17日 発行</p>
+                       <p class="f_bold">【主なトピック】</p>
+                       <div class="f_12">
+                           ★イケメン俳優の1日保育士体験★長江崚行
+                           ★様ざまな角度から保護者を支える保育園特集
+                           ★編集長対談シリーズ★歌手・タレント　鈴木亜美さん
+                           ★【新連載】ハピニコマスター★ヨンヨン先生の保育士・幼稚園教諭向けマネジメント講座
+                           ★保育を支える企業アワード2021発表！
+                       </div>
+                   </a>
+               </li>
+
+               <li class="matchHeight"> <a href="https://www.mirakuupremium.com/mirakuu/mirakuu36/" target="_blank">
+
+                       <div class="fix col_1_1 mb20">
+                           <div class="left_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/MIRAIKU_vol36.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                           </div>
+                           <div class="right_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/MIRAIKU_vol36-guest.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                           </div>
+                       </div>
+
+
+                       <p class="f_20 f_bold f_green mb10">
+                           MiRAKUU Vol.36 </p>
+                       <p class="mb10">2021年10月15日 発行</p>
+                       <p class="f_bold">【主なトピック】</p>
+                       <div class="f_12">
+                           ★イケメン俳優の1日保育士体験【番外編】★青木 瞭
+                           ★子どもの主体性を大事にする保育園特集
+                           ★編集長対談シリーズ★タレント　杉浦太陽さん
+                           ★教えて！川邉先生～子どものお悩み解決します（21）★歯科医 川邉研次先生
+                           ★絵本男子のおすすめ絵本
+                       </div>
+                   </a>
+               </li>
+
+               <li class="matchHeight"> <a href="https://www.mirakuupremium.com/mirakuu/mirakuu35/" target="_blank">
+
+                       <div class="fix col_1_1 mb20">
+                           <div class="left_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/2021/07/MIRAIKU_vol35.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                           </div>
+                           <div class="right_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/2021/07/hoiku_miraiku_20210715.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                           </div>
+                       </div>
+
+
+                       <p class="f_20 f_bold f_green mb10">
+                           MiRAKUU Vol.35 </p>
+                       <p class="mb10">2021年7月15日 発行</p>
+                       <p class="f_bold">【主なトピック】</p>
+                       <div class="f_12">★イケメン俳優の1日保育士体験(番外編)：松村 優
+                           ★編集長対談シリーズ：タレント・DJ・音楽プロデューサー　古坂大魔王
+                           ★研修に力を入れている保育園特集</div>
+                   </a>
+               </li>
+               <!--
+               <li class="matchHeight"> <a href="https://www.mirakuupremium.com/mirakuu/mirakuu34/" target="_blank">
+
+                       <div class="fix col_1_1 mb20">
+                           <div class="left_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/2021/05/MIRAIKU_vol34.jpg" class="attachment-large size-large" alt="" loading="lazy" srcset="wp-content/uploads/2021/05/MIRAIKU_vol34.jpg 233w, wp-content/uploads/2021/05/MIRAIKU_vol34-212x300.jpg 212w" sizes="(max-width: 233px) 100vw, 233px" /></figure>
+                           </div>
+                           <div class="right_area">
+                               <figure><img width="233" height="330" src="wp-content/uploads/2021/05/hoiku_miraiku_20210415.jpg" class="attachment-large size-large" alt="" loading="lazy" srcset="wp-content/uploads/2021/05/hoiku_miraiku_20210415.jpg 233w, wp-content/uploads/2021/05/hoiku_miraiku_20210415-212x300.jpg 212w" sizes="(max-width: 233px) 100vw, 233px" /></figure>
+                           </div>
+                       </div>
+
+
+                       <p class="f_20 f_bold f_green mb10">
+                           MiRAKUU Vol.34 </p>
+                       <p class="mb10">2021年4月15日 発行</p>
+                       <p class="f_bold">【主なトピック】</p>
+                       <div class="f_12">★イケメン俳優の1日保育士体験(番外編)：平野 宏周
+                           ★編集長対談シリーズ：タレント・歌手　hitomi
+                           ★五感力を鍛える保育園特集</div>
+                   </a>
+               </li>
+               <li class="matchHeight"> <a href="https://www.mirakuupremium.com/mirakuu/mirakuu33/" target="_blank">
+
+                   <div class="fix col_1_1 mb20">
+                     <div class="left_area">
+                       <figure><img width="120" height="170" src="wp-content/uploads/2021/05/MIRAIKU_vol33_02.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                     </div>
+                     <div class="right_area">
+                       <figure><img width="120" height="170" src="wp-content/uploads/2021/05/hoiku_miraiku_20210115_02-1.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                     </div>
+                   </div>
+
+
+                   <p class="f_20 f_bold f_green mb10">
+                     MiRAKUU Vol.33 </p>
+                   <p class="mb10">2021年1月15日 発行</p>
+                   <p class="f_bold">【主なトピック】</p>
+                   <div class="f_12">【主なトピック】
+                     ★イケメン俳優の1日保育士体験(番外編)：吉開 一生
+                     ★編集長対談シリーズ：芸人・絵本作家　西野亮廣
+                     ★一人ひとりの成長を日々実感できる小規模保育園特集</div>
+                 </a> </li>
+
+               <li class="matchHeight"> <a href="https://www.mirakuupremium.com/mirakuu/mirakuu32/" target="_blank">
+
+                   <div class="fix col_1_1 mb20">
+                     <div class="left_area">
+                       <figure><img width="120" height="170" src="wp-content/uploads/2020/10/MIRAIKU_vol32.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                     </div>
+                     <div class="right_area">
+                       <figure><img width="120" height="170" src="wp-content/uploads/2020/10/hoiku_miraiku_2020aujpg.jpg" class="attachment-large size-large" alt="" loading="lazy" /></figure>
+                     </div>
+                   </div>
+
+
+                   <p class="f_20 f_bold f_green mb10">
+                     MiRAKUU Vol.32 </p>
+                   <p class="mb10">2020年10月15日 発行</p>
+                   <p class="f_bold">【主なトピック】</p>
+                   <div class="f_12">【主なトピック】
+                     ★イケメン俳優の1日保育士体験(番外編)：平松 來馬
+                     ★編集長対談シリーズ：タレント　山田まりや
+                     ★困難な時代に負けず丁寧に向き合う保育をする保育園特集</div>
+                 </a>
+               </li>
+               -->
+           </ul>
+
+           <p class="btn_01"><a href="content">MiRAKUUバックナンバー 一覧</a></p>
+       </div>
+
+
+       <div class="fix col_2 mb80">
+           <div class="right_area sp_mb15">
+               <div class="line_dot">
+                   <p class="f_green">OurService 02</p>
+                   <p class="f_32 f_bold ls_2 f_green">MiRAKUUぷれみあむ</p>
+               </div>
+               <p class="f_20 f_bold ls_1 mb15">保育園経営者向けポータルサイト</p>
+               <p class="lh_2 mb15">
+                   保育業界は企業からの情報が届きづらく、商品・サービスに目を向ける機会がなかなかありません。しかし、団体・企業を積極的に活用することで園の煩雑な業務が軽減され、保育士が子どもに向き合う時間が増え、結果として保育の質の向上につながります。
+                   保育業界全体の質の向上のために、保育園、幼稚園を運営・経営している園長、施設長の「欲しい！」を集めました。</p>
+               <p class="seminar_btn t_right"><a href="/media/mirakuu.php#premium" class="f_green">MORE→</a></p>
+           </div>
+           <div class="left_area">
+               <p><img src="wp-content/themes/template03/img/top/img_04.jpg?12" alt=""></p>
+           </div>
+       </div>
+       <div class="fix col_2 mb80">
+           <div class="right_area sp_mb15">
+               <div class="line_dot">
+                   <p class="f_green">OurService 03</p>
+                   <p class="f_32 f_bold ls_2 f_green">絵本男子</p>
+               </div>
+               <p class="f_20 f_bold ls_1 mb15">イケメン絵本男子の読み聞かせ＆手遊び</p>
+               <p class="lh_2 mb15">公式サイトでの動画配信をはじめ、各地に絵本男子が出張して、店舗やショッピングセンターでのイベント企画を行います。
+                   また、企業とのコラボレーションや企業様の想いを込めたオリジナル絵本を企画・制作し、絵本男子の読み聞かせ動画配信＆リアルイベントや保育園・幼稚園へのキャラバン活動で子どもたちに直接配布しています。
+               </p>
+               <p class="seminar_btn t_right"><a href="ehondanshi.php" class="f_green">MORE→</a></p>
+           </div>
+           <div class="left_area">
+               <p><img src="wp-content/themes/template03/img/top/img_02.jpg" alt=""></p>
+           </div>
+       </div>
+      </div>
+      </section>
+
+
+    <div class="bg_contact">
+   <div class="site_wrapper indent_content">
+
+     <div class="fix col_2">
+
+       <div class="right_area">
+         <div class="pc_w_80">
+           <div class="t_center">
+             <h2 class="ttl_04 f_42">お問い合わせ</h2>
+           </div>
+           <p class="t_center con_tel_box"></p>
+           <div class="mail_side t_center"><a href="https://www.evoinf.co.jp/contact.php">
+               <p class="ls_2">ネットからのお問い合わせ</p>
+               <p class="f_26 f_bold ls_2">CONTACT FORM</p>
+             </a></div>
+           <p class="t_right ls_2 mb30">受付時間8:30～17:30（土日祝定休）</p>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>            	<!-- brandsIntro -->
+ <div id="brandsIntro">
+     <div class="container">
+         <ul class="row brandBox">
+             <li class="col-xs-6 col-sm-3 col-md-15">
+                 <a href="//www.kosodatedays.com/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_kosodatedays.png" alt="こそだてDAYS" class="img-responsive"></a>
+             </li>
+             <li class="col-xs-6 col-sm-3 col-md-15">
+                 <a href="//www.okoranai.com/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_shimazu.png" alt="怒らない伝道師" class="img-responsive"></a>
+             </li>
+             <li class="col-xs-6 col-sm-3 col-md-15">
+                 <a href="//www.wel.ne.jp/doc/feature/koudan/index.html" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_orine.png" alt="女流講談師" class="img-responsive"></a>
+             </li>
+             <li class="col-xs-6 col-sm-3 col-md-15">
+                 <a href="//www.wel.ne.jp/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_wel.png" alt="ウェル" class="img-responsive"></a>
+             </li>
+             <li class="col-xs-6 col-sm-3 col-md-15">
+                 <a href="https://www.jinriki.info/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_jinriki.png" alt="人力" class="img-responsive"></a>
+             </li>
+             <li class="col-xs-6 col-sm-3 col-md-15">
+                 <a href="https://www.mukashi.info/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_mukashi.png" alt="昔話絵巻" class="img-responsive"></a>
+             </li>
+             <li class="col-xs-6 col-sm-3 col-md-15">
+                 <a href="http://www.miraiku.co.jp/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_mirakuu.png" alt="MiRAKUU" class="img-responsive"></a>
+             </li>
+               <li class="col-xs-6 col-sm-3 col-md-15">
+                 <a href="http://ehondanshi.com/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_ehondanshi.png" alt="絵本男子" class="img-responsive"></a>
+             </li>
+           </ul>
+         <p class="footerCopy">Evolved Information 進化した情報の提供</p>
+     </div>
+ </div>
+ <!-- //brandsIntro -->
+<?php get_footer(); ?>
