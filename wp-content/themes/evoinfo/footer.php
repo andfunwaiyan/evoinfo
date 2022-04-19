33a2fd896a2fcf2b159006e2c5ce36ac15c7b597
LAPTOP-GIG08P28\asus <andfunwaiyan@gmail.com>
2022-04-19 11:18:56 +0630

    remove unnecessary files

---
+++ wp-content/themes/evoinfo/footer.php
@@ -0,0 +1,40 @@
+<!--footer-->
+<footer id="footer">
+        <div class="container">
+
+			
+            <div class=" col-xs-12 col-sm-8">
+                            <ul class="nav navbar-nav">
+                                    <li><a href="https://www.evoinf.co.jp/index.php">ホーム</a></li>
+                                    <li><a href="https://www.evoinf.co.jp/about.php">企業概要</a></li>
+                                    <li><a href="https://www.evoinf.co.jp/privacy.php">個人情報保護方針</a></li>
+                                    <li><a href="https://www.evoinf.co.jp/services.php">サービス概要</a></li>
+                                    <li><a href="https://www.evoinf.co.jp/contact.php">お問い合わせ</a></li>
+                            </ul>			
+            </div> 
+                        <div class="copyRight col-xs-12 col-sm-4">
+                <span>c2022 EvolvedInfo. All Rights Reserved.</span>
+            </div> 
+            <!--
+            <div class="socialLink col-xs-6 col-sm-2">
+                <ul class="nav navbar-nav">
+                    <li>
+                      <a href="#"><span class="symbol">circletwitterbird</span></a>
+                    </li>
+                    <li>
+                      <a href="#"><span class="symbol">circlefacebook</span></a>
+                    </li>
+                </ul>
+            </div> 
+            -->
+        </div>
+    </footer>
+    <!--//footer-->
+        <a href="#top" id="toTop"><img src="https://www.evoinf.co.jp/assets/img/totop.png" alt="TOPに戻る" width="35" height="41"></a>
+    <script type="text/javascript" src="https://www.evoinf.co.jp/assets/js/jquery.js"></script>
+    <script type="text/javascript" src="https://www.evoinf.co.jp/assets/js/bootstrap.js"></script>
+    <script type="text/javascript" src="https://www.evoinf.co.jp/assets/js/site.js"></script>
+
+</body>
+
+</html>
---
+++ wp-content/themes/evoinfo/front-page.php
@@ -0,0 +1,164 @@
+<?php wp_head(); ?>
+
+<!--globalNav-->
+
+<div class="globalNav">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+        </div>
+        <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+            <ul class="nav navbar-nav" style="margin:0 auto;">
+                                <li id="home">
+                                <a href="https://www.evoinf.co.jp/">ホーム</a>
+                                </li>
+                                <li id="about">
+                                <a href="https://www.evoinf.co.jp/about.php">企業概要</a>
+                                </li>
+                                <li id="service">
+                                  <a href="https://www.evoinf.co.jp/services.php">サービス概要</a>
+                                </li>
+                                <li id="contact">
+                                  <a href="https://www.evoinf.co.jp/contact.php">お問い合わせ</a>
+                                </li>
+            </ul>
+        </nav>
+    </div>
+</div>      
+<!--//globalNav-->    <!--mainVisual-->
+
+<div class="pageVisualWrapper visualToppage">
+    <div class="headerText container">
+        <div class="row">
+            <div class="visualToppageText col-sm-6 col-md-6">
+                <div class="title"><span class="evo">Evolved info</span><span>進化した情報の提供</span></div>
+                <div class="subTitle">この時代に生きた者として、後世に伝承すべきメッセージを残すことを企業理念としています。事業の推進においては、既成の伝達手段にとらわれることなく、また、今の時代に生きるものとして意識すべき社会問題や課題を踏まえた活動を行ってまいります。</div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--//mainVisual-->
+
+<section id="newsSection">
+    <div class="container">
+        <div class="row">
+            <h2 class="col-xs-12 col-sm-4">NEWS<span class="japanase">お知らせ</span></h2>
+            <dl class="col-xs-12 col-sm-8 row">
+                <dt class="col-xs-12 col-sm-2">2021.11.01</dt>
+                <dd class="col-xs-12 col-sm-10" style="margin-bottom: .8rem;">保育業界専門誌「<a href="http://www.miraiku.co.jp/">MiRAKUU</a>」と「<a href="http://ehondanshi.com/">絵本男子</a>」を運営している『メディア事業』が加わりました。</dd>
+                <dt class="col-xs-12 col-sm-2">2021.10.01</dt>
+                <dd class="col-xs-12 col-sm-10" style="margin-bottom: .8rem;">岐阜県高山市を舞台にした地方創生RPG『<a href="https://www.takayama-quest.com/">たかやまくえすと</a>』を弊社アカウントからリリースいたしました。</dd>
+                <dt class="col-xs-12 col-sm-2">2019.09.30</dt>
+                <dd class="col-xs-12 col-sm-10" style="margin-bottom: .8rem;">昔ばなし絵巻シリーズの<a href="https://www.mukashi.info/">絵本10作品</a>が完成しました。</dd>
+                <dt class="col-xs-12 col-sm-2">2017.05.22</dt>
+                <dd class="col-xs-12 col-sm-10" style="margin-bottom: .8rem;">育児中のママのあるある体験をマンガで紹介するサイト<br>『<a href="https://www.kosodatedays.com/">こそだてＤＡＹＳ</a>』をオープンしました。</dd>
+                <dt class="col-xs-12 col-sm-2">2015.04.10</dt>
+                <dd class="col-xs-12 col-sm-10">ハピニコマスター★ヨンヨン（嶋津良智/しまづよしのり）の新書<br>『<a href="https://www.amazon.co.jp/gp/product/4904703111/ref=as_li_qf_sp_asin_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4904703111&linkCode=as2&tag=evoinf0b-22" target="_blank">ハピニコマスター★ヨンヨンの7日間イラオコダイエット</a>』を発刊いたしました。</dd>
+
+            </dl>
+
+        </div>
+    </div>
+</section>
+
+<!--section-->
+<section id="topBusiness">
+    <div class="container">
+        <h2>SERVICE<span class="japanase">サービス概要</span></h2>
+        <div class="row ServiceBoxColumn">
+            <div class="serviceBox col-xs-12 col-sm-3 col-md-3">
+                <a href="https://www.evoinf.co.jp/services.php#contents" class="servicePanel">
+                    <div class="serviceImage">
+                        <img src="./assets/img/img_contents-jigyo.png" alt="コンテンツ事業" class="img-responsive">
+                    </div>
+                    <div class="serviceBody">
+                        <h3>コンテンツ事業</h3>
+                        <p>オリジナル絵本「昔ばなし絵巻シリーズ」の出版と書籍連動知育アプリの開発。子育て中の”共感”を漫画化したメディアサイト「こそだてDAYS」の運営、アンガーマネジメントを子育て向けに開発したメソッド“イラオコダイエット”書籍の出版、歴史旧街道をテーマにした健康ウォーキング情報サイトの運営等を行っています。
+                        </p>
+                    </div>
+                </a>
+
+            </div>
+            <div class="serviceBox col-xs-12 col-sm-3 col-md-3">
+                <a href="https://www.evoinf.co.jp/services.php#management" class="servicePanel">
+
+                    <div class="serviceImage">
+                        <img src="assets/img/img_event-management.png" alt="イベント/マネジメント事業" class="img-responsive">
+                    </div>
+                    <div class="serviceBody">
+                        <h3>イベント／マネジメント事業</h3>
+                        <p>教育コンサルタントとして累計150万部「怒らない技術」シリーズ（フォレスト出版社）のベストセラー作家でもある嶋津良智先生による「子育てセミナー」、成年後見制度の全国普及啓蒙を目的とした女流講談師神田織音師匠の「成年後見講談」等の運営、マネジメント事業などを行っています。</p>
+                    </div>
+                </a>
+            </div>
+            <div class="serviceBox col-xs-12 col-sm-3 col-md-3">
+                <a href="https://www.evoinf.co.jp/services.php#netAd" class="servicePanel">
+
+                    <div class="serviceImage">
+                        <img src="assets/img/img_netAd.jpg" alt="海外・その他事業" class="img-responsive">
+                    </div>
+                    <div class="serviceBody">
+                        <h3>ネット広告事業</h3>
+                        <p>インターネット広告・検索エンジンマーケティング・サイト制作などクライアント様のご要望を実現する為のデジタルマーケティングをご提供致します。PC、スマートフォン、タブレットなどあらゆるデバイスでのご提案が可能です。</p>
+                    </div>
+
+                </a>
+            </div>
+            <div class="serviceBox col-xs-12 col-sm-3 col-md-3">
+                <a href="https://www.evoinf.co.jp/media/" class="servicePanel">
+
+                    <div class="serviceImage">
+                        <img src="assets/img/media-top.png" alt="メディア事業" class="img-responsive">
+                    </div>
+                    <div class="serviceBody">
+                        <h3>メディア事業</h3>
+                        <p>1都3県の保育園・幼稚園約12,000施設、専門学校・大学に配布している園経営者・保育士・保育学生向けの保育業界専門誌『MiRAKUU』の発行、web版「MiRAKUU」、園長・施設長向け情報サイト「MiRAKUUぷれみあむ」の運営、イケメン絵本男子による絵本紹介・読み聞かせ・手遊び動画の配信を行っています。</p>
+                    </div>
+
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--//section-->
+
+    <!-- brandsIntro -->
+<div id="brandsIntro">
+    <div class="container">
+        <ul class="row brandBox">
+            <li class="col-xs-6 col-sm-3 col-md-15">
+                <a href="//www.kosodatedays.com/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_kosodatedays.png" alt="こそだてDAYS" class="img-responsive"></a>
+            </li>
+            <li class="col-xs-6 col-sm-3 col-md-15">
+                <a href="//www.okoranai.com/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_shimazu.png" alt="怒らない伝道師" class="img-responsive"></a>
+            </li>
+            <li class="col-xs-6 col-sm-3 col-md-15">
+                <a href="//www.wel.ne.jp/doc/feature/koudan/index.html" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_orine.png" alt="女流講談師" class="img-responsive"></a>
+            </li>
+            <li class="col-xs-6 col-sm-3 col-md-15">
+                <a href="//www.wel.ne.jp/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_wel.png" alt="ウェル" class="img-responsive"></a>
+            </li>
+            <li class="col-xs-6 col-sm-3 col-md-15">
+                <a href="https://www.jinriki.info/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_jinriki.png" alt="人力" class="img-responsive"></a>
+            </li>
+            <li class="col-xs-6 col-sm-3 col-md-15">
+                <a href="https://www.mukashi.info/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_mukashi.png" alt="昔話絵巻" class="img-responsive"></a>
+            </li>
+            <li class="col-xs-6 col-sm-3 col-md-15">
+                <a href="http://www.miraiku.co.jp/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_mirakuu.png" alt="MiRAKUU" class="img-responsive"></a>
+            </li>	            
+            <li class="col-xs-6 col-sm-3 col-md-15">
+                <a href="http://ehondanshi.com/" target="_blank"><img src="https://www.evoinf.co.jp/assets/img/brand_ehondanshi.png" alt="絵本男子" class="img-responsive"></a>
+            </li>
+        </ul>
+        <p class="footerCopy">Evolved Information 進化した情報の提供</p>
+    </div>
+</div>
+<!-- //brandsIntro -->
+
+<?php wp_footer(); ?>
